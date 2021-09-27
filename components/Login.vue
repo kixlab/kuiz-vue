ca57c79e1f44679e0ec727ec7e4cc60cada236e8
Kijoon <igreen0485@gmail.com>
2021-09-27 22:16:33 +0900

    login modified

--- components/Login.vue
+++ components/Login.vue
@@ -5,7 +5,7 @@
       You can sign in or register with your Google account.
     </Wrapper>
     <Wrapper px="6" style="margin: 28px 0">
-      <Wrapper rounded shadow class="row login" @click.native="logIn">
+      <Wrapper rounded shadow class="row login" @click.native="handleLogin">
         <div style="padding: 0 8px">
           <img src="~assets/images/brand-google.png" />
         </div>
@@ -17,10 +17,23 @@
 
 <script>
 import { mapMutations } from "vuex";
+// import { inject, toRefs } from "vue";
 
 export default {
+  data: () => ({
+  }),
   methods: {
     ...mapMutations(["logIn"]),
+    async handleLogin() {
+      try {
+        console.log("aaa");
+        const GoogleUser = await this.$gAuth.signIn();
+        console.log(GoogleUser);
+      } catch (e) {
+        console.log("errr");
+        console.error(e);
+      }
+    },
   },
 };
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/vue-google-oauth2'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
